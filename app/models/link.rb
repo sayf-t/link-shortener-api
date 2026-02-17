d65a3482974bcda23f318b6d3b7e42ba487e236f
sayf-t <sayfcodes@gmail.com>
2026-02-17 15:14:43 +0800

    Link and ClickEvent model specs

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,9 @@
 class Link < ApplicationRecord
+  has_many :click_events, dependent: :destroy
+
+  normalizes :target_url, with: ->(url) { url.to_s.strip }
+
+  validates :short_code, presence: true, uniqueness: true, length: { maximum: 15 }
+  validates :target_url, presence: true,
+                       format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: 'must be a valid HTTP or HTTPS URL' }
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with all required attributes' do
+      expect(Link.new(short_code: 'abc1234', target_url: 'https://test.com')).to be_valid
+    end
+
+    it 'requires a target_url' do
+      link = Link.new(short_code: 'abc1234', target_url: nil)
+      expect(link.valid?).to be false
+      expect(link.errors[:target_url]).to include "can't be blank"
+    end
+
+    it 'requires a short_code' do
+      link = Link.new(short_code: nil, target_url: 'https://test.com')
+      expect(link.valid?).to be false
+      expect(link.errors[:short_code]).to include "can't be blank"
+    end
+
+    it 'requires unique short_codes' do
+      Link.create!(short_code: 'abcd123', target_url: 'https://test.com')
+      link_2 = Link.new(short_code: 'abcd123', target_url: 'https://test.com')
+      expect(link_2.valid?).to be false
+      expect(link_2.errors[:short_code]).to include "has already been taken"
+    end
+
+    it 'requires max length of 15 on short_code' do
+      link = Link.new(short_code: 'a' * 16, target_url: 'https://test.com')
+      expect(link).not_to be_valid
+    end
+
+    it 'removes whitespace from target_url' do
+      link = Link.new(short_code: 'abc1234', target_url: '  https://test.com  ')
+      link.valid?
+      expect(link.target_url).to eq 'https://test.com'
+    end
+
+    it 'rejects URLs that are not http or https' do
+      expect(Link.new(short_code: 'abc1234', target_url: 'not-a-url')).not_to be_valid
+      expect(Link.new(short_code: 'abc1234', target_url: 'ftp://test.com')).not_to be_valid
+      expect(Link.new(short_code: 'abc1234', target_url: 'https://test.com')).to be_valid
+    end
+  end
 end
